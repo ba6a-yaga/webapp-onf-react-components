e40449784d5c5ce43d28cbe33aae1a2abaf0f309
wyndrick <wyndrick@gmail.com>
2020-01-26 16:20:33 +0200

    Работа над базой знаний

--- article-card/index.js
+++ article-card/index.js
@@ -4,20 +4,28 @@ import Avatar from '../avatar'
 export class ArticleCard extends Component {
 
     contentPreloader = [
-        {width:"60%", height:"14px", backgroundColor:"#F8F8F8", marginBottom:8},
-        {width:"55%", height:"14px", backgroundColor:"#F8F8F8", marginBottom:7},
+        {width:"100%", height:"204px", backgroundColor:"#F8F8F8", marginBottom:10},
+        {width:"85%", height:"15px", backgroundColor:"#BDBDBD", marginBottom:4},
+        {width:"95%", height:"15px", backgroundColor:"#BDBDBD", marginBottom:4},
+        {width:"15%", height:"15px", backgroundColor:"#BDBDBD", marginBottom:13},
+        {width:"50%", height:"42px", backgroundColor:"#F8F8F8", marginBottom:13, float:"left", borderRadius:"21px"},
+        {width:"25%", height:"21px", backgroundColor:"#F8F8F8", marginTop:10, float:"left", marginLeft:"12.5%"},
     ]
 
     onDelete(e) {
+        e.preventDefault();
         if (this.props.onDelete) {
-            this.props.onDelete(this.props.index)
+            this.props.onDelete(this.props.id)
         }
+        return false
     }
 
     render() {
         const {
-            name, 
-            avatar,
+            id, 
+            url, 
+            title,
+            text,
             className,
             isLoading
         } = this.props
@@ -31,20 +39,31 @@ export class ArticleCard extends Component {
                     className ? className: '',
                 ].filter(item=>item).join(' ')}
             >   
-                {!isLoading && <Avatar className="card__avatar" name={name} url={avatar}/>}
-                {!isLoading && <h2 className="card__name m-0">{name}</h2>}
-                {!isLoading && <div className="edit"></div>}
-                {!isLoading && <div className="delete" onClick={this.onDelete.bind(this)}></div>}
-                {isLoading &&
-                    <div className="member-card__preloader">
-                        <div className="member-card__preloader-circle"></div>
-                        <div>
-                            {this.contentPreloader.map((item)=>{
-                                return <div className="member-card__preloader-line" style={{...item}}></div>
-                            })}
-                        </div>  
-                    </div> 
-                }
+                    {!isLoading
+                        ? [
+                            <div class="card__content card__video">
+                                <iframe width="560" height="315" src={url} frameborder="0"
+                                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                                        allowfullscreen></iframe>
+                            </div>,
+                            <div class="card__text">
+                                <h2>{title}</h2>
+                                <p class="gray">{text}</p>
+                            </div>,
+                            <div class="card__button">
+                                <a href={`article/edit/${id}`} type="button" class="btn btn-edit btn-second">Редактировать</a>
+                                <a href="/" type="button"  onClick={this.onDelete.bind(this)} class="btn btn-third mr-4">Удалить</a>
+                            </div>
+                        ]
+                        : <div className="article-card__preloader">
+                                <div className="article-card__preloader-circle"></div>
+                                <div>
+                                    {this.contentPreloader.map((item)=>{
+                                        return <div className="article-card__preloader-line" style={{...item}}></div>
+                                    })}
+                                </div>  
+                            </div> 
+                        }
             </div>
         )
     }
--- articles-list/index.js
+++ articles-list/index.js
@@ -3,6 +3,7 @@ import "./index.scss"
 import ContentLoader from '../content-loader';
 import MemberCard from '../member-card';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import ArticleCard from '../article-card';
 export default class MembersList extends Component {
     preloadingItemsCount = 6;
     constructor(props) {
@@ -11,8 +12,11 @@ export default class MembersList extends Component {
             list: props.data ? props.data : [],
             isLoading:false,
             noMoreData:false,
+            columnsCount:3
         }
+        this.onResize = this.onResize.bind(this)
     }
+
 
     onLoadMore(e) { 
         console.log("LOADING MORE")
@@ -22,10 +26,18 @@ export default class MembersList extends Component {
             const length = this.state.list.length
             for (let i = 0; i < 9; i++) {
                 this.state.list.push({
-                    id:1022 + this.state.list.length + i,
-                    avatar:"https://politrussia.com/upload/iblock/795/7951c8694db4f913c633faece5d5de3e.jpg",
-                    name:"Иванов Василий Петрович",
-                    role:"Автор закупки"
+                    id:10 + this.state.list.length + i,
+                    url:"https://www.youtube.com/embed/S_dfq9rFWAE",
+                    title:"Активисты ОНФ выявили в «Городе N» — Бердске сомнительные закупки на сумму 260 млн рублей",
+                    text:`Активисты и эксперты проекта ОНФ «За честные закупки» в рамках инициативы «Город N»
+                    проверили
+                    закупки в
+                    городе Бердск Новосибирской области. В разряд сомнительных попали закупки на строительство и
+                    капитальный
+                    ремонт зданий и сооружений, благоустройство территорий и строительство дорог на общую сумму
+                    260
+                    млн руб.
+                    Всю информацию эксперты ОНФ передали в прокуратуру.`
                 })
             }
             console.log(this.state.list.length)
@@ -34,45 +46,90 @@ export default class MembersList extends Component {
         }, 1000)
     }
 
-    onDelete(index) {
-        this.state.list.splice(index, 1)
-        this.setState({list:this.state.list})
+    componentDidMount() {
+        window.addEventListener("resize", this.onResize)
     }
 
-    onAddMemberClick(e) {
+    onResize(e) {
+        const width = window.innerWidth
+        let currentColumnsCount = this.state.columnsCount;
+        let newColumnsCount = currentColumnsCount;
+        if (width < 768) {
+            newColumnsCount = 1
+        } else if (width < 992 ) {
+            newColumnsCount = 2
+        } else {
+            newColumnsCount = 3
+        }
+        if (currentColumnsCount != newColumnsCount) {
+            this.setState({columnsCount:newColumnsCount})
+        }
+    }
+
+    reorder = (arr, columns) => {
+        const cols = columns;
+        const out = [];
+        let col = 0;
+
+        console.log("reordering")
+        while(col < cols) {
+            for(let i = 0; i < arr.length; i += cols) {
+                let _val = arr[i + col];
+                if (_val !== undefined)
+                    out.push(_val);
+            }
+            col++;
+        }
+        return out
+    }
+
+    onDelete(id) {
+        let index = this.state.list.findIndex(item => {return item.id == id});
+        if (index != -1) {
+            this.state.list.splice(index, 1)
+            this.setState({list:this.state.list, columnsCount:this.state.columnsCount})
+        }
+    }
+
+    onAddClick(e) {
         
     }
 
-    render() {
+    getElements () {
         const {list, isLoading, noMoreData} = this.state
+        var elements = list.map((item, index) => {
+            return (
+            <div className="masonry__item mb-3" key={item.id}>
+                <ArticleCard 
+                    onDelete={(index) => {this.onDelete(index)}}
+                    {...item}
+                />
+            </div>
+        )})
+        if (isLoading) {
+            for (let i = 0; i < this.preloadingItemsCount; i++) {
+                elements.push(
+                    <div className="masonry__item mb-3">
+                        <ArticleCard isLoading={true}/>
+                    </div>
+                )
+            }
+        }
+        return elements
+    }
+    render() {
+        const {list, isLoading, noMoreData, columnsCount} = this.state
         return (
-            <div className="purchases-list container">
-            
-                <div className="row">
-                    {list.map((item, index) => {
-                        return (
-                        <div className="col-12 col-md-6 col-lg-4 mb-3" key={index}>
-                            <MemberCard 
-                                onDelete={(index) => {this.onDelete(index)}}
-                                {...item}
-                            />
-                        </div>
-                    )})}
-                    {isLoading && Array(this.preloadingItemsCount).fill().map((i, index) => {
-                        return (
-                            <div className="col-12 col-md-6 col-lg-4 mb-3">
-                                <MemberCard isLoading={true}/>
-                            </div>
-                        )
-                        })
-                    }
+            <div className="articles-list container">
+                <div className="masonry" style={{columnCount:columnsCount}}>
+                    {this.reorder(this.getElements(), columnsCount)}
                 </div>
 
                 <ContentLoader isLoading={isLoading} noMoreData={noMoreData} onLoadMore={this.onLoadMore.bind(this)} />
 
                 <div className="row mt-5">
                     <div className="col-12 text-center">
-                        <button type="button" onClick={this.onAddMemberClick.bind(this)} className="btn btn-main">Создать новый аккаунт</button>
+                        <button type="button" onClick={this.onAddClick.bind(this)} className="btn btn-main">Добавить видеоролик</button>
                     </div>
                 </div>
             </div>
